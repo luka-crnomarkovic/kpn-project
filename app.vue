77ab8d44fdcfb7cb10222c6173f6f15d8f69404a
Luka Crnomarkovic <l.crnomarkovic@vegait.rs>
2022-11-28 14:40:58 +0100

    Create phone component and add style with sass files and tailwind classes

--- app.vue
+++ app.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline text-center">KPN Mobilel shop</h1>
+    <h1 class="text-3xl font-bold underline text-center mb-10">
+      KPN Mobilel shop
+    </h1>
+    <div class="mobile-container flex justify-center flex-wrap">
+      <div
+        class="mobile-card flex justify-center flex-col flex-wrap"
+        v-for="item in phones"
+      >
+        <Phone :phone="item" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Phone from "./components/Phone.vue";
 
 export default {
   data() {
     return {
-      phones: [],
+      phones: null,
     };
   },
   methods: {
-    async getPhones() {
+    getPhones() {
       try {
-        await axios.get("http://localhost:3000/phones").then((res) => {
-          this.phones = res.data.products;
+        axios.get("http://localhost:3000/phones").then((res) => {
+          this.phones = JSON.parse(JSON.stringify(res.data.products));
         });
       } catch (error) {
         console.log("error", error);
---
+++ components/Phone.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img src="../static/images/phone.jpg" />
+    <div class="flex justify-between">
+      <div class="flex flex-col">
+        <span>{{ phone.manufacturer }}</span>
+        <span>{{ phone.name }}</span>
+      </div>
+      <div class="button">
+        <button>View</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    phone: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>

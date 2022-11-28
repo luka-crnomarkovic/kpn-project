5cc687bf8d8326a3c11e795e66cb515f84bf6562
Luka Crnomarkovic <l.crnomarkovic@vegait.rs>
2022-11-28 17:43:53 +0100

    Add filters, fix style on Phone component

--- app.vue
+++ app.vue
@@ -3,10 +3,74 @@
     <h1 class="text-3xl font-bold underline text-center mb-10">
       KPN Mobilel shop
     </h1>
+    <div v-show="phones" class="flex m-10 justify-center">
+      <div>
+        <span> Brand </span>
+        <div v-for="(brand, index) in brands" :key="index">
+          <input
+            type="checkbox"
+            :id="brand"
+            :value="brand"
+            v-model="checkedBrands"
+          />
+          <label class="ml-2" :for="brand">{{ brand }}</label>
+        </div>
+      </div>
+      <div class="ml-5">
+        <span> Have 5g? </span>
+        <div v-for="(fiveG, index) in fiveGOptions" :key="index">
+          <input
+            type="checkbox"
+            :id="fiveG.label"
+            :value="fiveG.value"
+            v-model="checkedFiveG"
+          />
+          <label class="ml-2" :for="fiveG">{{ fiveG.label }}</label>
+        </div>
+      </div>
+      <div class="ml-5">
+        <span> Operating system </span>
+        <div v-for="(operatingSystem, index) in operatingSystems" :key="index">
+          <input
+            type="checkbox"
+            :id="operatingSystem"
+            :value="operatingSystem"
+            v-model="checkedOperatingSystems"
+          />
+          <label class="ml-2" :for="operatingSystem">{{
+            operatingSystem
+          }}</label>
+        </div>
+      </div>
+      <div class="ml-5">
+        <span> E-sim </span>
+        <div v-for="(eSim, index) in eSimOptions" :key="index">
+          <input
+            type="checkbox"
+            :id="eSim.label"
+            :value="eSim.value"
+            v-model="checkedESim"
+          />
+          <label class="ml-2" :for="eSim">{{ eSim.label }}</label>
+        </div>
+      </div>
+      <div class="ml-5">
+        <span> Refurbished </span>
+        <div v-for="(refurbished, index) in refurbishedOptions" :key="index">
+          <input
+            type="checkbox"
+            :id="refurbished.label"
+            :value="refurbished.value"
+            v-model="checkedRefurbishedOptions"
+          />
+          <label class="ml-2" :for="refurbished">{{ refurbished.label }}</label>
+        </div>
+      </div>
+    </div>
     <div class="mobile-container flex justify-center flex-wrap">
       <div
-        class="mobile-card flex justify-center flex-col flex-wrap"
-        v-for="item in phones"
+        class="mobile-card flex justify-center flex-col flex-wrap rounded-xl m-2"
+        v-for="item in displayedPhones"
       >
         <Phone :phone="item" />
       </div>
@@ -22,16 +86,160 @@ export default {
   data() {
     return {
       phones: null,
+      brands: [],
+      hasFiveG: [],
+      fiveGOptions: [
+        { label: "Yes", value: true },
+        { label: "No", value: false },
+      ],
+      eSimOptions: [
+        { label: "Yes", value: true },
+        { label: "No", value: false },
+      ],
+      refurbishedOptions: [
+        { label: "Yes", value: true },
+        { label: "No", value: false },
+      ],
+      operatingSystems: [],
+      checkedBrands: [],
+      checkedFiveG: [],
+      checkedOperatingSystems: [],
+      checkedESim: [],
+      checkedRefurbishedOptions: [],
+      displayedPhones: [],
     };
+  },
+  watch: {
+    checkedBrands() {
+      this.brandFilter();
+    },
+    checkedFiveG() {
+      this.fiveGFilter();
+    },
+    checkedOperatingSystems() {
+      this.operatingSystemsFilter();
+    },
+    checkedESim() {
+      this.eSimFilter();
+    },
+    checkedRefurbishedOptions() {
+      this.refurbishedFilter();
+    },
   },
   methods: {
     getPhones() {
       try {
         axios.get("http://localhost:3000/phones").then((res) => {
           this.phones = JSON.parse(JSON.stringify(res.data.products));
+          this.displayedPhones = this.phones;
+          this.getBrands();
+          this.getOperatingSystems();
         });
       } catch (error) {
         console.log("error", error);
+      }
+    },
+    getBrands() {
+      this.brands = this.phones.map((el) => el.manufacturer);
+      let unique = [...new Set(this.brands)];
+      this.brands = unique;
+    },
+    getOperatingSystems() {
+      this.operatingSystems = this.phones.map((el) => el.operating_system);
+      let unique = [...new Set(this.operatingSystems)];
+      this.operatingSystems = unique;
+    },
+
+    brandFilter() {
+      if (this.checkedBrands.length !== 0) {
+        let phones = this.phones;
+        let brands = this.checkedBrands;
+        const myArrayFiltered = phones.filter((phone) => {
+          return brands.some((brand) => {
+            return brand === phone.manufacturer;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
+      }
+    },
+
+    fiveGFilter() {
+      if (this.checkedFiveG.length !== 0) {
+        let phones = this.phones;
+        let fiveGOptions = this.checkedFiveG;
+
+        const myArrayFiltered = phones.filter((phone) => {
+          return fiveGOptions.some((brand) => {
+            return brand === phone.has_5g;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
+      }
+    },
+
+    fiveGFilter() {
+      if (this.checkedFiveG.length !== 0) {
+        let phones = this.phones;
+        let fiveGOptions = this.checkedFiveG;
+
+        const myArrayFiltered = phones.filter((phone) => {
+          return fiveGOptions.some((brand) => {
+            return brand === phone.has_5g;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
+      }
+    },
+
+    operatingSystemsFilter() {
+      if (this.checkedOperatingSystems.length !== 0) {
+        let phones = this.phones;
+        let operatingSystems = this.checkedOperatingSystems;
+
+        const myArrayFiltered = phones.filter((phone) => {
+          return operatingSystems.some((operatingSystem) => {
+            return operatingSystem === phone.operating_system;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
+      }
+    },
+    eSimFilter() {
+      if (this.checkedESim.length !== 0) {
+        let phones = this.phones;
+        let eSimOptions = this.checkedESim;
+
+        const myArrayFiltered = phones.filter((phone) => {
+          return eSimOptions.some((brand) => {
+            return brand === phone.has_esim;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
+      }
+    },
+    refurbishedFilter() {
+      if (this.checkedRefurbishedOptions.length !== 0) {
+        let phones = this.phones;
+        let refurbishedOptions = this.checkedRefurbishedOptions;
+
+        const myArrayFiltered = phones.filter((phone) => {
+          return refurbishedOptions.some((brand) => {
+            return brand === phone.refurbished;
+          });
+        });
+        this.displayedPhones = myArrayFiltered;
+      } else {
+        this.displayedPhones = this.phones;
       }
     },
   },
--- components/Phone.vue
+++ components/Phone.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <img src="../static/images/phone.jpg" />
-    <div class="flex justify-between">
+    <div class="flex justify-between p-2">
       <div class="flex flex-col">
         <span>{{ phone.manufacturer }}</span>
-        <span>{{ phone.name }}</span>
+        <span class="text-sm">{{ phone.name }}</span>
       </div>
-      <div class="button">
+      <div class="button flex justify-center rounded-lg p-1">
         <button>View</button>
       </div>
     </div>

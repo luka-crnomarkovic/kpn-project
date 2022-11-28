1f0622bfbfa3dc5f32175edb2896b257e281a051
Luka Crnomarkovic <l.crnomarkovic@vegait.rs>
2022-11-28 12:50:48 +0100

    Add axios and get phones with get request

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
+    <h1 class="text-3xl font-bold underline text-center">KPN Mobilel shop</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      phones: [],
+    };
+  },
+  methods: {
+    async getPhones() {
+      try {
+        await axios.get("http://localhost:3000/phones").then((res) => {
+          this.phones = res.data.products;
+        });
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
+  },
+  mounted() {
+    this.getPhones();
+  },
+};
+</script>
